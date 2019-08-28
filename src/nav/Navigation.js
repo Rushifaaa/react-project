dd1c7fbb20f14579c234f9ad510efd7ab6ead8bd
unknown <jakub.gencer@allaboutapps.at>
2019-08-28 08:00:06 +0200

    Added a NavBar with routing

--- src/nav/Navigation.js
+++ src/nav/Navigation.js
@@ -1,18 +1,28 @@
-import React, { Component } from 'react';
-import { BrowserRouter, Router, Route, Link } from 'react-router-dom';
+import React from 'react';
+import { Route, Link, BrowserRouter as Router } from 'react-router-dom';
+import './Navigation.css';
 
-class Navigation extends Component {
+//Component Imports
+import App from './../home/App';
+import About from './../about/About'
+
+class Navigation extends React.Component {
     render() {
         return (
-            <Router>
-               <div class="nav-div">
-                   <ul>
-                       <li>
-                           <Route exact to="/">Home</Route>
-                       </li>
-                   </ul>
-               </div>
-            </Router>
+            <nav>
+                <navItem>
+
+                </navItem>
+            </nav>
+            <Link to="/"><p><strong>Home</strong></p></Link>
+            <Link to="/about"><p><strong>About</strong></p></Link>
+
+            
+            <Route exact path="/" component={App} />
+            <Route path="/about" component={About} />
+            
         );
     }
+    
 }   
+export default Navigation;
